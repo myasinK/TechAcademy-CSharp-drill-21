b970a6986842295b82af043b0adf283827521942
myasinK <yasinul@gmail.com>
2018-08-17 11:54:07 -0700

    Finish struct intro

---
+++ StructIntro/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructIntro
+{
+    public struct Number
+    {
+        public decimal Amount;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number number = new Number();
+            number.Amount = 45.34356M;
+            Console.WriteLine(number.Amount);
+        }
+    }
+}
